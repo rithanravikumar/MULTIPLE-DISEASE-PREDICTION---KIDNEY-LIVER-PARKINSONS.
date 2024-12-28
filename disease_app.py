3912cb61a73571cd97d503d5abf9f904e782a5c9
rithanravikumar <rithanravikumar@gmail.com>
2024-12-28 13:26:46 +0530

    Update disease_app.py

--- disease_app.py
+++ disease_app.py
@@ -4,21 +4,24 @@ import numpy as np
 
 # Load models
 def load_modelp():
-    with open("parkinsons.pkl", "rb") as f:
+    with open("https://github.com/rithanravikumar/MULTIPLE-DISEASE-PREDICTION---KIDNEY-LIVER-PARKINSONS./raw/refs/heads/main/parkinsons.pkl
+", "rb") as f:
         return pickle.load(f)
 model2 = load_modelp()
 
 # with open('/mnt/data/parkinsons.pkl', 'rb') as file:
 #     parkinsons_model = pickle.load(file)
 def load_modelk():
-    with open("kidney.pkl", "rb") as f:
+    with open("https://github.com/rithanravikumar/MULTIPLE-DISEASE-PREDICTION---KIDNEY-LIVER-PARKINSONS./raw/refs/heads/main/kidney.pkl
+", "rb") as f:
         return pickle.load(f)
 model1K = load_modelK()
 
 # with open('/mnt/data/kidney.pkl', 'rb') as file:
 #     kidney_model = pickle.load(file)
 def load_modelL():
-    with open("liver.pkl", "rb") as f:
+    with open("https://github.com/rithanravikumar/MULTIPLE-DISEASE-PREDICTION---KIDNEY-LIVER-PARKINSONS./raw/refs/heads/main/liver.pkl
+", "rb") as f:
         return pickle.load(f)
 model1L = load_modelK()
 
